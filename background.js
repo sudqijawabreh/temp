87699d6962c4a9c7338a59fb3833c3ca059468bd
sudqi jawabreh <sudqi jawabreh>
2016-05-28 09:35:16 +0300

    fix indentation,add .tern-project for autocomplete

--- background.js
+++ background.js
@@ -1,108 +1,104 @@
 chrome.storage.local.get('[inComingCourses,oldCourses]',function(items){
-  if(items.inComingCourses!=undefined)inComingCourses=items.inComingCourses;
-  if(items.courseId!=undefined)courseId=items.oldCourses;
+    if(items.inComingCourses!==undefined)inComingCourses=items.inComingCourses;
+    if(items.courseId!==undefined)courseId=items.oldCourses;
 });
-  chrome.webRequest.onBeforeSendHeaders.addListener(
+chrome.webRequest.onBeforeSendHeaders.addListener(
 
         function(details) {
-          var newValue="";
-        
-        
-         
-         
+            var newValue="";
             if(details.url=="http://www2.najah.edu/gradarch/default.asp"){
-              newValue="http://www2.najah.edu/gradarch/default.asp";
+                newValue="http://www2.najah.edu/gradarch/default.asp";
             }
-        details.requestHeaders.push({
-        name: 'Referer',
-        value: newValue,
-    });
- //       details.requestHeaders.push({
- //       name: 'Content-Type',
- //       value: "application/x-www-form-urlencoded; charset=UTF-8",
- //   });  
-    return {
-        requestHeaders: details.requestHeaders
-    };
-       },
+            details.requestHeaders.push({
+                name: 'Referer',
+                value: newValue,
+            });
+            //       details.requestHeaders.push({
+            //       name: 'Content-Type',
+            //       value: "application/x-www-form-urlencoded; charset=UTF-8",
+            //   });  
+            return {
+                requestHeaders: details.requestHeaders
+            };
+        },
         {urls: ["http://www2.najah.edu/gradarch/default.asp"]},
         ["blocking", "requestHeaders"]);
- 
 
-        name="";
-        avg=0;
-        high=100;
-        low=0;
 
- chrome.browserAction.onClicked.addListener(function(){
-     for(var i=0;i<100;i++){
-         var s=getStudentFromServer();
-         console.log(s.name);
-         console.log(s.id);
-         var avg=(binaryGeuss(name,low,high));
-         console.log(avg);
-         setStudentAvg(s,avg);
-     } 
-	 });
+name="";
+avg=0;
+high=100;
+low=0;
+
+chrome.browserAction.onClicked.addListener(function(){
+    for(var i=0;i<100;i++){
+        var s=getStudentFromServer();
+        console.log(s.name);
+        console.log(s.id);
+        var avg=(binaryGeuss(name,low,high));
+        console.log(avg);
+        setStudentAvg(s,avg);
+    } 
+});
 function getStudentFromServer(){
-	  var xhr= new XMLHttpRequest();
-	  xhr.open("POST","http://localhost/getName.php",false);
-      xhr.send();
-              name=$(xhr.responseText).filter('input').first().val();
-              var id =$(xhr.responseText).filter('input').next().val();
-            var student=new Object();
-              student.id=id;
-              student.name=name;
-              return student;
+    var xhr= new XMLHttpRequest();
+    xhr.open("POST","http://localhost/getName.php",false);
+    xhr.send();
+    name=$(xhr.responseText).filter('input').first().val();
+    var id =$(xhr.responseText).filter('input').next().val();
+    var student={};
+    student.id=id;
+    student.name=name;
+    return student;
 }
 function setStudentAvg(student,avg){
-	  var r= new XMLHttpRequest();
-	  r.open("POST","http://localhost/setAvg.php",true);
-      r.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	  r.send("id="+student.id+"&avg="+avg);
+    var r= new XMLHttpRequest();
+    r.open("POST","http://localhost/setAvg.php",true);
+    r.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    r.send("id="+student.id+"&avg="+avg);
 }
 function requestEqualAvg(name,avg){
-//	  var xhr= new XMLHttpRequest();
-//	  xhr.open("POST","http://localhost/getName.php",false);
-//      xhr.send();
-//              name=$(xhr.responseText).filter('input').val();
-      var r= new XMLHttpRequest();
-      r.open("POST","http://www2.najah.edu/gradarch/default.asp",false);
+    //	  var xhr= new XMLHttpRequest();
+    //	  xhr.open("POST","http://localhost/getName.php",false);
+    //      xhr.send();
+    //              name=$(xhr.responseText).filter('input').val();
+    var r= new XMLHttpRequest();
+    r.open("POST","http://www2.najah.edu/gradarch/default.asp",false);
     r.setRequestHeader("Content-Type","application/x-www-form-urlencoded;UTF-8");
-      r.onreadystatechange=function(){
-          if(r.readyState==4 && r.status==200){
-          }
-      }
+    r.onreadystatechange=function(){
+        if(r.readyState==4 && r.status==200){
+        }
+    };
     var string="%' and stu_avg="+avg+" and stu_nam <>'%";
     string=encodeURI(string);
-      r.send("multiple=2&stuNam="+name+string);
-      return r.responseText;
+    r.send("multiple=2&stuNam="+name+string);
+    return r.responseText;
 
 }
 function requestAboveAvg(name,avg){
-//	  var xhr= new XMLHttpRequest();
-//	  xhr.open("POST","http://localhost/getName.php",false);
-//      xhr.send();
-//              name=$(xhr.responseText).filter('input').val();
-      var r= new XMLHttpRequest();
-      r.open("POST","http://www2.najah.edu/gradarch/default.asp",false);
+    //	  var xhr= new XMLHttpRequest();
+    //	  xhr.open("POST","http://localhost/getName.php",false);
+    //      xhr.send();
+    //              name=$(xhr.responseText).filter('input').val();
+    var r= new XMLHttpRequest();
+    r.open("POST","http://www2.najah.edu/gradarch/default.asp",false);
     r.setRequestHeader("Content-Type","application/x-www-form-urlencoded;UTF-8");
-      r.onreadystatechange=function(){
-          if(r.readyState==4 && r.status==200){
-          }
-      }
+    r.onreadystatechange=function(){
+        if(r.readyState==4 && r.status==200){
+        }
+    };
     var string="%' and stu_avg>="+avg+" and stu_nam <>'%";
     string=encodeURI(string);
-      r.send("multiple=2&stuNam="+name+string);
-      return r.responseText;
+    r.send("multiple=2&stuNam="+name+string);
+    return r.responseText;
 
 }
 function isEqualAvg(name,avg){
-        str=encodeToWindows1256(name.trim());
+    str=encodeToWindows1256(name.trim());
     var page=requestEqualAvg(str,avg);
     var table=getTable(page);
     var rows=getRows(table); if(rows.length==1){
-        return false;
+      
     }
     else{
         for(var i=1;i<rows.length;i++){
@@ -116,7 +112,7 @@ function isEqualAvg(name,avg){
 
 }
 function isAboveAvg(name,avg){
-        str=encodeToWindows1256(name.trim());
+    str=encodeToWindows1256(name.trim());
     var page=requestAboveAvg(str,avg);
     var table=getTable(page);
     var rows=getRows(table);
@@ -136,35 +132,35 @@ function isAboveAvg(name,avg){
 }
 function binaryGeuss(name,left,right){
     var mid=(right+left)/2.0;
-    mid=Number(Math.round(mid+'e'+2)+'e-'+2);
+        mid=Number(Math.round(mid+'e'+2)+'e-'+2);
     if(mid<0.5)return -1;
     equal=null;
     var above=null;
     var below=null;
     while(equal==null){
-    try{
-    var equal =isEqualAvg(name,mid);
-    }
-    catch(e){
-        setTimeout(function(){},1000);
-    }
+        try{
+            var equal =isEqualAvg(name,mid);
+        }
+        catch(e){
+            setTimeout(function(){},1000);
+        }
     }
 
     console.log(mid);
     if(equal==true){
         return mid;
-    console.log(mid);
+        console.log(mid);
     }
     else if(isAboveAvg(name,mid)){
         console.log('right');
         while(true){
-        try{
-        return binaryGeuss(name,mid,right);
-        }
-        catch(e){
-        setTimeout(function(){},1000);
-               console.log('faild request'); 
-        }
+            try{
+                return binaryGeuss(name,mid,right);
+            }
+            catch(e){
+                setTimeout(function(){},1000);
+                console.log('faild request'); 
+            }
         }
     }
     else{
@@ -176,8 +172,8 @@ function binaryGeuss(name,left,right){
                 return binaryGeuss(name,left,mid);
             }
             catch(e){
-               console.log('faild request'); 
-        setTimeout(function(){},1000);
+                console.log('faild request'); 
+                setTimeout(function(){},1000);
             }
         }
 
@@ -189,49 +185,49 @@ function getThemAll(){
             getPage(i,j);
         }
     }
-        for(var j=1;j<37;j++){
-            getPage(21,j);
-        }
-        for(var j=1;j<37;j++){
-            getPage(39,j);
-        }
-        for(var j=1;j<37;j++){
-            getPage(40,j);
-        }
-    
+    for(var j=1;j<37;j++){
+        getPage(21,j);
+    }
+    for(var j=1;j<37;j++){
+        getPage(39,j);
+    }
+    for(var j=1;j<37;j++){
+        getPage(40,j);
+    }
+
 }
 function getPage(colCod,fawjCod){
-      var r= new XMLHttpRequest();
-      r.open("POST","http://www2.najah.edu/gradarch/default.asp",true);
-      r.send("multiple=3&colCod="+colCod+"&fawjCod="+fawjCod);
-        r.onreadystatechange=function(){
+    var r= new XMLHttpRequest();
+    r.open("POST","http://www2.najah.edu/gradarch/default.asp",true);
+    r.send("multiple=3&colCod="+colCod+"&fawjCod="+fawjCod);
+    r.onreadystatechange=function(){
 
 
-          if(r.readyState==4&&r.status==200){
+        if(r.readyState==4&&r.status==200){
             extractPage(r.responseText);
-         }
         }
+    }
 }
 function extractPage(page){
     var table=getTable(page);
     var rows=getRows(table);
     var students=[];
     for(var i=1;i<rows.length;i++){
-       var student=rowToJson(rows[i]); 
-       students[i-1]=student;
+        var student=rowToJson(rows[i]); 
+        students[i-1]=student;
     }
     sendStudents(students);
 }
 function sendStudents(students){
-	  var r= new XMLHttpRequest();
-	  r.open("POST","http://localhost/database.php",true);
-	    r.onreadystatechange=function(){
+    var r= new XMLHttpRequest();
+    r.open("POST","http://localhost/database.php",true);
+    r.onreadystatechange=function(){
 
-	      if(r.readyState==4&&r.status==200){
-	     }
-	 };
-r.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	  r.send("students="+JSON.stringify(students));
+        if(r.readyState==4&&r.status==200){
+        }
+    };
+    r.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    r.send("students="+JSON.stringify(students));
 
 }
 function rowToJson(row){
@@ -244,7 +240,6 @@ function rowToJson(row){
     year=$(columns[4]).text();
     fawj=$(columns[5]).text();
     birthPlace=$(columns[6]).text();
-
     student={"name":name,"collage":collage,"spec":spec,"semester":semester,"fawj":fawj,"birth_place":birthPlace,"year":year};
     return student;
 }
